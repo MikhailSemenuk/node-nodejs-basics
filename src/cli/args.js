6434893411f025f4223084a9c60a91c677d0dd86
Maksim Shylau <shilaums@gmail.com>
2022-04-17 18:38:30 +0200

    feat: solve cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,29 @@
 const parseArgs = () => {
-    // Write your code here 
+
+    let argv = process.argv.slice(2);
+
+    const getKey = (str) => {
+        if (str.startsWith('--')) {
+            return str.slice('--'.length)
+        }
+        return undefined;
+    }
+
+    let keyValue = [];
+    argv.forEach(element => {
+        const key = getKey(element);
+        if (key !== undefined) {
+            keyValue.push([key, undefined]);
+        } else {
+            keyValue.at(-1)[1] = element;
+        }
+
+    });
+
+    const answer = keyValue.map(([key, value]) => `${key} is ${value}`).join(', ')
+
+    console.log(answer);
 };
+
 
 parseArgs();
