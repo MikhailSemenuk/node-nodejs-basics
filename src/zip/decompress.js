c926d9a96dcfdfad0d57ad8759338c5ea9e934e1
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-10-06 21:28:54 +0200

    feat: solve zip tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { getCurrentFolder } from "../fs/libs.js";
+import { createReadStream, createWriteStream } from "fs";
+import { join } from "node:path";
+import { createGunzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const gunzip = createGunzip()
+
+    const fileFrom = join(getCurrentFolder(import.meta.url), "./files/archive.gz");
+    const fileTo = join(getCurrentFolder(import.meta.url), "./files/fileToCompress.txt");
+
+    const readStream = createReadStream(fileFrom);
+    const writeStream = createWriteStream(fileTo)
+
+    readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
