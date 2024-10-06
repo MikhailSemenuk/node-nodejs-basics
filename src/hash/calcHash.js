21c6be7b45ba13fc1b1512c942a125e8dfa057a3
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-10-06 21:05:53 +0200

    feat: solve hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { getCurrentFolder } from "../fs/libs.js";
+import { createReadStream } from "fs";
+import { join } from "node:path";
+import { createHash } from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const filePath = join(getCurrentFolder(import.meta.url), "./files/fileToCalculateHashFor.txt");
+
+    const readStream = createReadStream(filePath);
+    readStream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log(hash.digest('hex'));
+    })
 };
 
 await calculateHash();
