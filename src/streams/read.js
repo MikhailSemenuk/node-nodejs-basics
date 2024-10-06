81014c0c2d81c2549dc7652bb7025602e976d884
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-10-06 17:02:31 +0200

    feat: solve streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { EOL } from "node:os";
+import { getCurrentFolder } from "../fs/libs.js";
+import { createReadStream } from "fs";
+import { join } from "node:path";
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(getCurrentFolder(import.meta.url), "./files/fileToRead.txt");
+    const readStream = createReadStream(filePath);
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk + EOL);
+    });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { EOL } from 'os';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().split('').reverse().join('') + EOL;
+            callback(null, reversed);
+        }
+    });
+
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
