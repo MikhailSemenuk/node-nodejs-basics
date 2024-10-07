986a9e249b9c1e9a414f05f1137a6436aad523a0
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-10-07 19:30:35 +0200

    feat: solve cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { spawn } from "child_process";
+import { getCurrentFolder } from "../fs/libs.js";
+import { join } from "node:path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = join(getCurrentFolder(import.meta.url), "./files/script.js");
+
+    const childProcess = spawn('node', [scriptPath, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
